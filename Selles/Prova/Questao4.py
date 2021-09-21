6b041f397a0f142b667b426831ca74b74238ca7f
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-09-21 13:45:26 -0300

    Excluindo 4° Questão

--- Selles/Prova/Questao4.py
+++
@@ -1,6 +0,0 @@
-size = float(input('Qual é o tamanho do arquivo que você deseja baixar?(Digite em MB) '))
-download = float(input('Digite a velocidade da sua internet em Mbps: '))
-
-MBps = download/8
-tempo = MBps/size
-print(f'{tempo/60} Minutos')
