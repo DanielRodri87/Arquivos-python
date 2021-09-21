f82b12d82a37bb1cfecb1ede261428fea81a9e87
Jean Carlos <86329183+JeanCarlos899@users.noreply.github.com>
2021-09-21 14:02:38 -0300

    Add files via upload

---
+++ Selles/Prova/q8.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite o nummero 1: "))
+num2 = int(input("Digite o nummero 2: "))
+
+print("Soma: ", num1+num2)
+print("Subtração: ", num1-num2)
+print("Divisão: ", num1/num2)
+print("Divisão inteira : ", num1//num2)
+print("Resto: ", num1%num2)
