4392cd7dc80cc502a2a16e81ab1d6450234bb2f9
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-09-22 19:55:22 -0300

    Def revisão

--- mundo3/dev.py
+++ mundo3/dev.py
@@ -1,7 +1,29 @@
-def daniel():
-    print(40*'*')
+
+# def l():
+#     print('#'*55)
 
 
-daniel()
-print('oi')
-daniel()
+# l()
+# print('oi')
+# print('Eu sou o Daniel')
+# print('E tenho 16 anos')
+# l()
+
+###############################################################################################
+# def soma(a,b):
+#     s = a+b
+#     print(s)
+
+
+# soma(1,3)
+# soma(4,5)
+
+##############################################
+def contador(*núm):
+    print(núm)
+    print(f'O tamanho é: {len(núm)}')
+
+
+contador(1,2,1,7)
+contador(8,7,8,6,5,4,3,6,4,3)
+contador(4,0)
