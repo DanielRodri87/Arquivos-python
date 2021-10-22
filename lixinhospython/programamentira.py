aec5ac8fe034d67c3c0935a909c072016f92deb5
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-10-22 16:49:33 -0300

    mentira

---
+++ lixinhospython/programamentira.py
@@ -0,0 +1,9 @@
+
+frase = str(input('Digite uma afirmação: '))
+fonte = str(input("Quem lhe disse isso? ")).upper
+
+if fonte == 'MARCOS':
+    print('Está informação é mentirosa')
+
+else:
+    print('Pode ser que seja verdade. ')
