fce12e60a997561cf7d820039e48a01907342024
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-11-10 16:20:45 -0300

    sql

---
+++ MySQL/arquivo_teste.sql
@@ -0,0 +1,18 @@
+-- Sejam bem-vindos a aula
+
+-- Comando para abrir o terminal
+-- mysql -u root -p
+
+-- Primeiro passo
+-- Criar uma base de dados
+
+CREATE DATABASE aula_bd_2ano;
+
+-- Acessar as bases de dado
+
+-- Criar uma tabela
+
+
+-- Como instalar o MySQL
+-- https://www.youtube.com/watch?v=7Q-_X-_Xz-Q
+
