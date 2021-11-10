0a922b6542e8ba7adee0474cdc7f97a0cfebac36
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-11-10 15:42:54 -0300

    MySQL

--- Rodolfo_Python/Aula_repetições.py
+++ Rodolfo_Python/Aula_repetições.py
@@ -1,13 +1,4 @@
-def questao():
-    print("Questão 1")
-    print("1. Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.")
-    numero = int(input("Digite um número inteiro: "))
-    contador = 0
-    for i in range(1, numero + 1):
-        if numero % i == 0:
-            contador += 1
-    if contador == 2:
-        print("O número digitado é primo.")
-    else:
-        print("O número digitado não é primo.")
-    print("")
+# Como imprimir uma lista ao contrário
+lista = ["Carro","bicicleta","moto"]
+for i in range(len(lista)-1,-1,-1):
+    print(lista[i])
