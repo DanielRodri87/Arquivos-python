b01f27d8f9c19a82302543d23b5437641512ad80
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-11-13 12:20:30 -0300

    POO att

--- Py_Orientado_Obj/casses.py
+++ Py_Orientado_Obj/casses.py
@@ -2,7 +2,8 @@ from pessoa import Pessoa
 
 p1 = Pessoa("Daniel", 16)
 p2 = Pessoa("Jean", 16)
+p3 = Pessoa("Selma", 18)
 # Mesma coisa de p1.nome = "Daniel"
 
-p1.xingar(p2.nome)
+p1.xingar(p3.nome)
 
