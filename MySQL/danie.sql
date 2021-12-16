741cc53e2c1e69fdcd14059b67b386809ff6f11f
DanielRodri87 <daniel.acaua.piaui.brasil@gmai.com>
2021-12-16 09:05:21 -0300

    att

---
+++ MySQL/danie.sql
@@ -0,0 +1,28 @@
+CREATE TABLE games(
+id INT(7) KEY AUTO_INCREMENT, 
+nome VARCHAR(80) NOT NULL,
+data_lancamento DATE NOT NULL,
+preco DOUBLE(6,2) NOT NULL
+);
+
+INSERT INTO games (id, nome, data_lancamento, preco)
+VALUES
+(1, "Pes21", "2020-09-15",262.00);
+
+INSERT INTO games (id, nome, data_lancamento, preco)
+VALUES
+(2, "Pes20", "2019-09-10",30.00);
+
+INSERT INTO games (id, nome, data_lancamento, preco)
+VALUES
+(3, "Clash Royale", "2016-03-02", 0.00);
+
+INSERT INTO games (id, nome, data_lancamento, preco)
+VALUES
+(4, "Clash Of Clans", "2012-08-02", 0.00);
+
+INSERT INTO games (id, nome, data_lancamento, preco)
+VALUES
+(5, "Free Fire", "2017-08-23", 0.00);
+
+-- como chamar o mysql: mysql -u root -p
