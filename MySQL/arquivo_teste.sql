46551d006a6e056fdc0db4427a2d137d7a4f929d
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-12-16 17:51:13 -0300

    att

--- MySQL/arquivo_teste.sql
+++ MySQL/arquivo_teste.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS `arquivo_teste2`;
-USE `arquivo_teste2`;
+CREATE DATABASE IF NOT EXISTS `arquivo_teste3`;
+USE `arquivo_teste3`;
 
 CREATE TABLE games(
 id INT(7) KEY AUTO_INCREMENT,
@@ -10,23 +10,23 @@ preco DOUBLE(6,2) NOT NULL
  
 INSERT INTO games (id, nome, data_lancamento, preco)
 VALUES
-(1, "Pes21", "2020-09-15", 262.00)
+(1, "Pes21", "2020-09-15", 262.00);
  
 INSERT INTO games (id, nome, data_lancamento, preco)
 VALUES
-(2, "Pes20", "2019-09-10", 30.00)
+(2, "Pes20", "2019-09-10", 30.00);
  
 INSERT INTO games (id, nome, data_lancamento, preco)
 VALUES
-(3, "Clash Royale", "2016-03-02", 0.00)
+(3, "Clash Royale", "2016-03-02", 0.00);
  
 INSERT INTO games (id, nome, data_lancamento, preco)
 VALUES
-(4, "Clash Of Clans", "2012-08-02", 0.00)
+(4, "Clash Of Clans", "2012-08-02", 0.00);
  
 INSERT INTO games (id, nome, data_lancamento, preco)
 VALUES
-(5, "Free Fire", "2017-08-23", 0.00)
+(5, "Free Fire", "2017-08-23", 0.00);
 
 -- Imprimir tabela
 SELECT * FROM games;
