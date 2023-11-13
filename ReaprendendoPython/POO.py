3f13ba30b59f102f2361b701c685f869d48e8994
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-13 14:39:28 -0300

    att

---
+++ ReaprendendoPython/POO.py
@@ -0,0 +1,15 @@
+class Pessoa:
+    def __init__(self, nome, rg, cpf, sexo):
+        self.nome = nome
+        self.rg = rg
+        self.cpf = cpf
+        self.sexo = sexo
+        
+    
+    def DefinirNome(nome):
+        return nome
+    
+    
+    
+pessoaname = Pessoa.DefinirNome(nome="Daniel")
+        
